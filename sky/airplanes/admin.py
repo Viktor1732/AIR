31dcdccdbd5eb8edbabfc7662816eea212f94826
Viktor1732 <programmer.kras@gmail.com>
2023-01-24 16:38:20 +0700

    - Прописал автозаполнение слага, фильтры по времени создания и публикации, и чекбокс для пункта публикации.

--- sky/airplanes/admin.py
+++ sky/airplanes/admin.py
@@ -7,12 +7,16 @@ class AirplaneAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'time_create', 'photo', 'is_published')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
+    list_editable = ('is_published',)
+    list_filter = ('is_published', 'time_create')
+    prepopulated_fields = {'slug': ("title",)}
 
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
     list_display_links = ('id', 'name')
     search_fields = ('id', 'name')
+    prepopulated_fields = {'slug': ('name',)}
 
 
 admin.site.register(Airplane, AirplaneAdmin)
