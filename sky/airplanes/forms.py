63b7649b337cfa98001a1eeaa933ab8e2e2c0737
Viktor1732 <programmer.kras@gmail.com>
2023-01-27 21:20:03 +0700

    - Добавил валидатор для полей title и slug.

--- sky/airplanes/forms.py
+++ sky/airplanes/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 from .models import Airplane
 
@@ -8,10 +9,21 @@ class AddAirplaneForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         self.fields['cat'].empty_label = 'Категория не выбрана'
 
-
     class Meta:
         model = Airplane
         fields = ['title', 'slug', 'content', 'photo', 'is_published', 'cat']
         widgets = {'title': forms.TextInput(attrs={'class': 'form-input'}),
                    'content': forms.Textarea(attrs={'cols': 50, 'rows': 10})
                    }
+
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if len(title) > 50:
+            raise ValidationError('Длина превышает 50 символов.')
+        return title
+
+    def clean_slug(self):
+        slug = self.cleaned_data['slug']
+        if len(slug) > 20:
+            raise ValidationError('Длина превышает 20 символов.')
+        return slug
