a01ed05724dae87a3f18c55e08da3528402300f7
Viktor1732 <programmer.kras@gmail.com>
2023-01-28 19:52:47 +0700

    - Убрал дублирование кода в views.py при помощи миксина DataMixin.

--- sky/airplanes/views.py
+++ sky/airplanes/views.py
@@ -5,18 +5,18 @@ from django.views.generic import ListView, DetailView, CreateView
 
 from .forms import AddAirplaneForm
 from .models import Airplane
+from .utils import DataMixin
 
 
-class AirplaneHome(ListView):
+class AirplaneHome(DataMixin, ListView):
     model = Airplane
     template_name = 'airplanes/index.html'
     context_object_name = 'posts'
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Главная страница'
-        context['cat_selected'] = 0
-        return context
+        c_def = self.get_user_context(title='Главная страница')
+        return dict(list(context.items()) + list(c_def.items()))
 
     def get_queryset(self):
         return Airplane.objects.filter(is_published=True)
@@ -26,15 +26,15 @@ def about(request):
     return render(request, 'airplanes/about.html', {'title': 'О сайте'})
 
 
-class AddPost(CreateView):
+class AddPost(DataMixin, CreateView):
     form_class = AddAirplaneForm
     template_name = 'airplanes/add_page.html'
     success_url = reverse_lazy('home')
 
     def get_context_data(self, **kwargs):
-        form = super().get_context_data(**kwargs)
-        form['title'] = 'Добавить статью'
-        return form
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Добавление статьи')
+        return dict(list(context.items()) + list(c_def.items()))
 
 
 def contact(request):
@@ -45,7 +45,7 @@ def login(request):
     return HttpResponse("<h1>ВХОД</h1>")
 
 
-class ShowPost(DetailView):
+class ShowPost(DataMixin, DetailView):
     model = Airplane
     context_object_name = 'post'
     template_name = 'airplanes/show_post.html'
@@ -53,8 +53,8 @@ class ShowPost(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = context['post']
-        return context
+        c_def = self.get_user_context(title=context['post'])
+        return dict(list(context.items()) + list(c_def.items()))
 
 
 def show_post(request, post_slug):
@@ -66,7 +66,7 @@ def show_post(request, post_slug):
     return render(request, 'airplanes/show_post.html', context=context)
 
 
-class AirplaneCategory(ListView):
+class AirplaneCategory(DataMixin, ListView):
     model = Airplane
     template_name = 'airplanes/index.html'
     context_object_name = 'posts'
@@ -77,9 +77,9 @@ class AirplaneCategory(ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Категория' + str(context['posts'][0].cat)
-        context['cat_selected'] = context['posts'][0].cat_id
-        return context
+        c_def = self.get_user_context(title='Категория' + str(context['posts'][0].cat),
+                                      cat_selected=context['posts'][0].cat_id)
+        return dict(list(context.items()) + list(c_def.items()))
 
 
 def pageNotFound(request, exception):
